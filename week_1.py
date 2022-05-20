498d4f8ded2802e59f6f31c392cb0a42041ab452
SM Abdullah <sm.abdullah@chisholm.edu.au>
2022-05-20 15:15:19 +1000

    week 1

---
+++ week_1.py
@@ -0,0 +1,20 @@
+# print('Python club -- week 1')
+
+# variable
+# name = 100
+# place = 200
+
+# print(name)
+# print(place)
+
+# print(name, place, sep=',')
+
+# taking input from user
+number_one = input("Enter the first number: ")
+number_two = input("Enter the second number: ")
+
+# print(float(number_one) + float(number_two))
+result = float(number_one) + float(number_two)
+print(f'This is the sum: {result:.2f}')
+
+# print("This is the sum" + str(result))
